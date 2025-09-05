c30cea52840b4b489e43a8157f52cc2426d784f6
Anindita Roy <aninroy06@gmail.com>
2025-09-06 00:59:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -85,6 +85,7 @@ def api_get(path, params=None):
 def refresh_matches(mode="live"):
     data = api_get(f"matches/v1/{mode}")
     st.write("DEBUG: API Response", data)
+    matches = data.get("typeMatches", [])
     """Fetch matches (live or recent) from RapidAPI Cricbuzz"""
     if mode == "live":
         data = api_get("matches/v1/live")
