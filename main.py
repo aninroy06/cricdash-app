4845dc37bad39eb10b1b803db6f2ceb7002d2b5a
Anindita Roy <aninroy06@gmail.com>
2025-09-06 00:15:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ init_db()
 menu = ["Live", "Players", "SQL Lab", "Admin"]
 choice = st.sidebar.radio("Navigate", menu)
 
-if choice == "Live":
+    if choice == "Live":
     st.header("Matches")
 
     match_mode = st.radio("Select match type", ["Live", "Recent"], horizontal=True)
